62340494f6d25f73a66b132cb7c684af72288e23
beba2123 <antenehbeba@gmail.com>
2023-10-31 21:52:15 +0300

    importing module for wait for database python file

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,8 +1,11 @@
 """
 Django command to wait for the database to be available.
 """
+from psycopg2 import OperationalError as Psycopg2Error
 
 from django.core.management.base import BaseCommand
+import time
+from django.db.utils import OperationalError
 
 class Command(BaseCommand):
     """ Django command to wait for database."""
