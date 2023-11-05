174711478085f0aa0f4758bed815e150e672ede9
beba2123 <antenehbeba@gmail.com>
2023-11-05 18:18:23 +0300

    set User model as default model & migration to db

--- app/app/settings.py
+++ app/app/settings.py
@@ -127,3 +127,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'core.User' #for telling to use User Model in my app name core file and moodels.py file.
