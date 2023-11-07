05a4d72b651f83a11fc8f6e41959e9eb8681336d
beba2123 <antenehbeba@gmail.com>
2023-11-07 21:47:30 +0300

    customize the admin

--- app/core/admin.py
+++ app/core/admin.py
@@ -4,5 +4,14 @@ Django admin customization.
 
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from core import models
 
 # Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    """Define the admin pages for users."""
+    ordering = ['id']
+    list_display = ('email', 'name')
+
+
+admin.site.register(models.User, UserAdmin) #to make customizable otherwise if we write it without UserAdmin it will be default one.
