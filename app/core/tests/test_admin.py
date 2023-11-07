01838fdd956f3408d273a856f369f12deb13e570
beba2123 <antenehbeba@gmail.com>
2023-11-07 21:40:17 +0300

    write a test for listed user

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -11,4 +11,24 @@ class AdminSiteTests(TestCase):
     "Tests for Django Admin."
 
     def setup(self):
-        """"""
+        """Create User and client."""
+        self.client = Client() #allows as http request(put, get..)
+        self.admin_user = get_user_model().objects.create_superuser(
+            email='admin@gmail.com',
+            password='password123',
+        )
+        self.client.force_login(self.admin_user) #for authentice each request made by client with the admin user
+        self.user = get_user_model().objects.create_user(
+            email='user@gmail.com',
+            password='password123',
+            name='Test User',
+        )
+
+    def test_users_list(self):
+        """Test that users are listed on user page."""
+
+        url = reverse('admin:core_user_changelist')  #get the list of all users in core app
+        res=self.client.get(url) #get the url from the request as a responce
+
+        self.assertContains(res, self.user.name)   #check if our created user is present in the list
+        self.assertContains(res, self.user.email)
