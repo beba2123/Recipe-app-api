17bbfe4135d492580d8456af99066d206e16a7b5
beba2123 <antenehbeba@gmail.com>
2023-11-09 20:27:03 +0300

    set fields user for django ad-user edit section

--- app/core/admin.py
+++ app/core/admin.py
@@ -5,6 +5,7 @@ Django admin customization.
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from core import models
+from django.utils.translation import gettext_lazy as _  # import translation for diffrent languages in Django
 
 # Register your models here.
 
@@ -12,6 +13,18 @@ class UserAdmin(BaseUserAdmin):
     """Define the admin pages for users."""
     ordering = ['id']
     list_display = ('email', 'name')
-
+    fieldsets=(
+        (None, {'fields': ('email', 'password')}),
+        (
+            _('permission'),
+            {
+                'fields':(
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                )
+            }
+        )
+    )
 
 admin.site.register(models.User, UserAdmin) #to make customizable otherwise if we write it without UserAdmin it will be default one.
