a293c5f899c4f6566a6e0c158b651090aadeeea7
beba2123 <antenehbeba@gmail.com>
2023-11-14 22:48:19 +0300

    writ test for password incorrect not to give token

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -66,3 +66,24 @@ class PublicUserApiTests(TestCase):
 
         self.assertIn('token', res.data)
         self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_create_token_bad_credentials(self):
+        """Test that token is not created when invalid credentials are given."""
+        create_user(email='test@example',password='password123')
+        payload = {'email':'test@example',
+                   'password': 'badpass',
+        }
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertNotIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    # def  test_create_token_blank_password(self):
+    #     """Test that token is not created when blank password is provided."""
+    #     payload ={'email':'test@example',
+    #               'password': '',
+    #     }
+
+    #     res = self.client.post(TOKEN_URL, payload)
+    #     self.assertIn('token', res.data)
+    #     self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
