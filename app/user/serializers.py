ec4281d662088076b926167edf58b0e4391aa699
beba2123 <antenehbeba@gmail.com>
2023-11-15 11:33:59 +0300

    serialize email & password first

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -1,7 +1,8 @@
 """
 Serializers for the user API View.
 """
-from django.contrib.auth import get_user_model
+from django.contrib.auth import ( get_user_model, authenticate)
+from django.utils.translation import gettext as _
 from rest_framework import serializers
 
 class UserSerializer(serializers.ModelSerializer):
@@ -15,4 +16,27 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         """Create  and return a user with encrypted password."""
         return get_user_model().objects.create_user(**validated_data)
-    
+
+class AuthTokenSerializer(serializers.Serializers): #import the default one
+    """Serializer for the user authentication token."""
+    email = serializers.EmailField()
+    password = serializers.CharField(
+        style = {'input_type': 'password'},
+        trim_whitespace =False,
+    )
+
+    # def validate(self, attrs):
+    #     """Validate and authenticate the user."""
+    #     email = attrs.get('email')
+    #     password = attrs.get('password')
+    #     user = authenticate(
+    #         request=self.context.get('request'),
+    #         email=email,
+    #         password=password,
+    #     )
+    #     if not user:
+    #         msg = _('Unable to authenticate with the provided credentials.')
+    #         raise serializers.ValidationError(msg, code='authentication') #it is a way/method to show that it is bad request through serialilzer
+
+    #     attrs['user'] = user #attrs contain a kev value pair where 'user' maps to the user data.
+    #     return attrs
