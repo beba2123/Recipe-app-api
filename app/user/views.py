7ea4b2f72d31df43d2777ab6b0a9643370d19148
beba2123 <antenehbeba@gmail.com>
2023-11-24 15:05:30 +0300

    write manage user view for user connect throug url

--- app/user/views.py
+++ app/user/views.py
@@ -1,7 +1,7 @@
 """
 Views for the user API.
 """
-from rest_framework import generics
+from rest_framework import generics, authentication , permissions
 # it provides generics class based views that simplify the implementation of common patterns.
 from user.serializers import UserSerializer, AuthTokenSerializer
 from rest_framework.authtoken.views import ObtainAuthToken
@@ -16,3 +16,13 @@ class CreateTokenView(ObtainAuthToken): #it has a defualt username and password
         """Create a new auth token for  users """
         serializer_class =  AuthTokenSerializer #wecustomize the serializer by changing to email and password from usrname and password
         renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+class ManageUserView(generics.RetrieveUpdateAPIView): #to use get, put and patch inside the class.
+      """Manage the authenticated user"""
+      serializer_class = UserSerializer
+      authentication_classes  =  [authentication.TokenAuthentication] #for authenticate the user tring to retrive or update
+      permission_classes = [permissions.IsAuthenticated] #permmite  the authenticated user
+
+      def get_object(self):
+            """Retrieve and return authenticated user"""
+            return self.request.user
