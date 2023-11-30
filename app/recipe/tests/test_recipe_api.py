5ea7ac7d21f1e9bda063d5fe97fdfe11ae9620f9
beba2123 <antenehbeba@gmail.com>
2023-11-30 16:36:08 +0300

    write test for authenticated user in a recipe api

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -8,7 +8,7 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIClient
 from core.models import Recipe
-
+from recipe.serializers import RecipeSerializer
 
 RECIPES_URL = reverse('recipe:recipe-list')
 
@@ -37,3 +37,25 @@ class PublicRecipesApiTests(TestCase):
         res = self.client.get(RECIPES_URL)
 
         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+class PrivateRecipesApiTests(TestCase):
+    """For authenticated user API tests."""
+
+    def setup(self):
+        self.client = APIClient
+        self.user = get_user_model().objects.create_user(
+            'user@gmail.com',
+            'password123',
+        )
+        self.client.force_authenticate(self.user)
+
+    def test_retrieve_recipes(self):
+        """Test retrieving a list of recipes."""
+
+        create_recipe(user=self.user)
+        create_recipe(user=self.user) # create two recipe from authenticated user.
+        res  = self.client.get(RECIPES_URL)
+
+        recipes =  Recipe.objects.all().order_by('-id')
+        serializer = RecipeSerializer(recipes, many=True)  #for serializing multiple instance but if we serialize single  instance 'many=false'
+
