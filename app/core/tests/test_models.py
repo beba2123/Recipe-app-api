1eac317bce53ff693053552fbc773b39ba603d93
beba2123 <antenehbeba@gmail.com>
2023-12-12 15:06:52 +0300

    test for create tag by check string tag equal to tag name

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -11,6 +11,7 @@ from django.contrib.auth import get_user_model
 def create_user(email='user@gmail.com', password='password123'):
     """create and return new user."""
     return get_user_model().objects.create_user(email, password)
+
 class ModelTests(TestCase):
     """Test models."""
     def test_create_user_with_email_successful(self):
@@ -66,3 +67,11 @@ class ModelTests(TestCase):
             description='Sample recipe description.',
         )
         self.assertEqual(str(recipe), recipe.title)
+
+
+    def test_create_tag(self):
+        """Test tag created correctly"""
+        user = create_user()
+        tag = models.Tag.objects.create(user=user, name='Tag1')
+
+        self.assertEqual(str(tag), tag.name)
