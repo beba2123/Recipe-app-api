31615c62ba303f93c407f481613f58771bed5fe9
beba2123 <antenehbeba@gmail.com>
2023-12-15 08:21:33 +0300

    create test for private tags & write test for retriev tags

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -4,7 +4,7 @@ from django.contrib.auth import get_user_model
 from django.urls import reverse
 from django.test import TestCase
 from rest_framework import status
-from rest_framework.test import APITestCase
+from rest_framework.test import APIClient
 from core.models import Tag, Recipe
 from recipe.serializers import TagSerializer
 
@@ -18,11 +18,29 @@ class PublicTagsApiTests(TestCase):
     """test for unauthorized API requests."""
 
     def setUp(self):
-        self.client = APITestCase()
+        self.client = APIClient()
 
     def test_auth_required(self):
         """Test that authentication is required to access this API endpoint."""
         res = self.client.get(TAGS_URL)
 
         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
-    
+
+class PrivateTagsApiTests(TestCase):
+
+    def setUp(self):
+        self.user = create_user()
+        self.client = APIClient()
+        self.client.force_authenticate(self.user)
+
+    def test_retrieve_tags(self):
+        """Test retrieving a list of tags."""
+        Tag.objects.create(user=self.user, name="Vegan")
+        Tag.objects.create(user=self.user, name="Dessert")
+
+        res = self.client.get(TAGS_URL)
+        tags = Tag.objects.all().order_by('-name')
+        serlizer  = TagSerializer(tags, many=True)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serlizer.data)
