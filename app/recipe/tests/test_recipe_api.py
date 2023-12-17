f03dab581a1c23432931d4d59eb610753af825fa
beba2123 <antenehbeba@gmail.com>
2023-12-17 13:59:23 +0300

    test for creating new tag and new recipe together

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -7,7 +7,7 @@ from django.test import TestCase
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIClient
-from core.models import Recipe
+from core.models import Recipe,  Tag
 from recipe.serializers import RecipeSerializer, RecipeDetailSerializer
 
 RECIPES_URL = reverse('recipe:recipe-list')
@@ -178,3 +178,23 @@ class PrivateRecipesApiTests(TestCase):
 
         self.assertEqual(res.status_code, status.HTTP_404_NOT_FOUND)
         self.assertTrue(Recipe.objects.filter(id=recipe.id).exists())
+
+    def test_create_recipe_with_new_tags(self):
+        """Test creating a recipe with tags."""
+        payload = {
+            'title': 'Thai Prawn Curry',
+            'time_minutes': 30,
+            'price':Decimal('2.50'),
+            'tags': [{'name':'thai'}, {'name':'Dinner'}],
+        }
+        res = self.client.post(RECIPES_URL, payload, format='json')
+
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+
+        recipes = Recipe.objects.filter(user=self.user)
+        recipe = recipes[0]
+        self.assertEqual(len(payload['tags']), len(recipe.tags)) #checking if the amount of tags that are in the payload is equal to recipe.
+        for tag in payload['tags']:
+            exists = recipe.tags.filter(name=tag['name'],user=self.user,).exists()
+            self.assertEqual(exists)
+            
