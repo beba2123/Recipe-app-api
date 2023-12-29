8ac966df885b3668869497f7d5f280c3be264eca
beba2123 <antenehbeba@gmail.com>
2023-12-29 11:11:59 +0300

    test for update recipe by assigning exising an ingredient

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -320,20 +320,20 @@ class PrivateRecipesApiTests(TestCase):
         new_ingredient = Ingredient.objects.get(user=self.user, name='Limes')
         self.assertIn(new_ingredient, recipe.ingredients.all())
 
-    # def test_update_recipe_assign_ingredient(self):
-    #     """Test assigning an existing ingredient when updating a recipe."""
-    #     ingredient = Ingredient.objects.create(user=self.user, name='pepper')
-    #     recipe = create_recipe(user=self.user)
-    #     recipe.ingredients.add(ingredient)
+    def test_update_recipe_assign_ingredient(self):
+        """Test assigning an existing ingredient when updating a recipe."""
+        ingredient = Ingredient.objects.create(user=self.user, name='pepper')
+        recipe = create_recipe(user=self.user)
+        recipe.ingredients.add(ingredient)
 
-    #     new_ingredient = Ingredient.objects.create(user=self.user, name='salt')
-    #     payload = {'ingredients':[{'name': 'salt'}]}
-    #     url = detail_url(recipe.id)
-    #     res = self.client.patch(url, payload, format='json')
+        new_ingredient = Ingredient.objects.create(user=self.user, name='salt')
+        payload = {'ingredients':[{'name': 'salt'}]}
+        url = detail_url(recipe.id)
+        res = self.client.patch(url, payload, format='json')
 
-    #     self.assertEqual(res.status_code, status.HTTP_200_OK)
-    #     self.assertIn(new_ingredient, recipe.ingredients.all())
-    #     self.assertNotIn(ingredient, recipe.ingredients.all())
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertIn(new_ingredient, recipe.ingredients.all())
+        self.assertNotIn(ingredient, recipe.ingredients.all())
 
     # def test_clear_recipe_ingredients(self):
     #     """Test removing all ingredients from a recipe."""
