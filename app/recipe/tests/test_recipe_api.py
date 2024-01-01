72d6d687cb643ca940ea5baedaf04609bda48199
beba2123 <antenehbeba@gmail.com>
2024-01-01 13:29:06 +0300

    test for filtering tags from recipe

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -355,7 +355,26 @@ class PrivateRecipesApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(recipe.ingredients.count(), 0)
 
+    def test_filter_by_tags(self):
+        """Test returning recipes with specific tags"""
+        tag1 = Tag.objects.create(user=self.user, name="Vegan")
+        tag2 = Tag.objects.create(user=self.user, name="Vegetarian")
+        recipe1 = create_recipe(self.user)
+        recipe2 = create_recipe(self.user)
+        recipe1.tags.add(tag1)
+        recipe2.tags.add(tag2)
 
+        recipe3 = create_recipe(user=self.user, title='chekena')
+        params = {'tags': f'{tag1.id}, {tag2.id}'}
+        res = self.client.get(RECIPES_URL, params)
+        #we have to serilize it tho change it into json format
+        serializer1 = RecipeSerializer(recipe1)
+        serializer2 = RecipeSerializer(recipe2)
+        serializer3 = RecipeSerializer(recipe3)
+        #check if the data exist in data
+        self.assertIn(serializer1.data, res.data)
+        self.assertIn(serializer2.data, res.data)
+        self.assertNotIn(serializer3.data, res.data)
 
 class ImageUploadTests(TestCase):
     """Test for image upload API."""
